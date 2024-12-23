fbe8c4e01c1df92b1d9a03ff2dde4ad8bf7ed1fa
hungpv <phamvanhung.dev@gmail.com>
2024-12-23 10:41:36 +0700

    done

--- base/browser.py
+++ base/browser.py
@@ -1,12 +1,13 @@
-from selenium import webdriver
+from seleniumwire import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import tempfile
-
+from time import sleep
 class Browser:
     def __init__(self,account = 'hung'):
         self.account = account
-        self.profile_dir = tempfile.mkdtemp(prefix=f"account_{account}_")
+        if account != 'hung':
+            self.profile_dir = tempfile.mkdtemp(prefix=f"account_{account}_")
         
     def start(self):
         chrome_options = Options()
@@ -14,28 +15,27 @@ class Browser:
             chrome_options.add_argument(f"--user-data-dir={self.profile_dir}")
         chrome_options.add_argument("--disable-notifications") 
         chrome_options.add_argument("--disable-geolocation")
-        chrome_options.add_argument("--use-fake-ui-for-media-stream")
-        chrome_options.add_argument("--disable-popup-blocking") 
-        chrome_options.add_argument("--incognito")
-        chrome_options.add_argument("--start-maximized")
-        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
-        chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        chrome_options.add_experimental_option('useAutomationExtension', False)
+        chrome_options.add_argument('--ignore-certificate-errors')
+        chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 
         service = Service('chromedriver.exe') 
-        browser = webdriver.Chrome(service=service,options=chrome_options)
+        # browser = webdriver.Chrome(service=service,options=chrome_options)
         
-        # Che giấu thuộc tính `webdriver`
-        browser.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {
-            'source': '''
-                Object.defineProperty(navigator, 'webdriver', {
-                    get: () => undefined
-                });
-            '''
-        })
+        proxy_username = '5KFr01yS823ZxMd'
+        proxy_password = 'cDIeYuzHd9ip9ZC'
+        proxy = "62.164.255.151:49178"
+
+        sw_options = {
+            'proxy': {
+                'http': f'http://{proxy_username}:{proxy_password}@{proxy}',
+                'https': f'https://{proxy_username}:{proxy_password}@{proxy}',
+            }
+        }
         
-        return browser
+        driver = webdriver.Chrome(service=service,options=chrome_options,seleniumwire_options=sw_options)
+
+        return driver
+        # return browser
     
     def cleanup(self):
         """Xóa thư mục tạm nếu được tạo."""
